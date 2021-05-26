f894626fbb43d5ee6bd744fa0dd7da29dd433ae2
Mayk Brito <maykbrito@gmail.com>
2021-05-26 13:00:46 -0300

    chore: update Omni colors

--- src/utils/EditorCustomTheme.ts
+++ src/utils/EditorCustomTheme.ts
@@ -4,20 +4,26 @@ export const omniTheme: editor.IStandaloneThemeData = {
   inherit: true,
   base: 'vs-dark',
   colors: {
-    'editor.foreground': '#E1E1E6',
-    'editor.background': '#191622',
+    'editor.foreground': '#E1E1E6', // bright
+    'editor.background': '#191622', // dark
   },
   rules: [
-    { token: 'type.identifier.js', foreground: '#67e480' },
+    // pink
     { token: 'delimiter.js', foreground: '#FF79C6' },
-    { token: 'property.js', foreground: '#9999aa' },
-    { token: 'delimiter.parenthesis.js', foreground: '#9999aa' },
     { token: 'keyword', foreground: '#FF79C6' },
     { token: 'tag', foreground: '#FF79C6' },
-    { token: 'attribute.value.html', foreground: '#e7de79' },
+    // green
+    { token: 'type.identifier.js', foreground: '#67e480' },
+    { token: 'attribute.value.css', foreground: '#67e480' },
+    // gray
+    { token: 'property.js', foreground: '#9999aa' },
+    { token: 'delimiter.parenthesis.js', foreground: '#9999aa' },
+    // purple
     { token: 'metatag.html', foreground: '#988bc7' },
     { token: 'metatag.xml', foreground: '#988bc7' },
+    // yellow
+    { token: 'attribute.value.html', foreground: '#e7de79' },
     { token: 'string.css', foreground: '#e7de79' },
-    { token: 'attribute.value.css', foreground: '#67e480' },
+    { token: 'string.js', foreground: '#e7de79' },
   ],
 };
