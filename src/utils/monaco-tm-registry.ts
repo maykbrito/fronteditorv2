fc132bff76883283db6abb5f5e37338e92c53978
Diego Fernandes <diego.schell.f@gmail.com>
2022-05-24 18:19:41 -0300

    fix: Load grammer files using ?url suffix

--- src/utils/monaco-tm-registry.ts
+++ src/utils/monaco-tm-registry.ts
@@ -1,34 +1,31 @@
 import { Registry } from 'monaco-textmate'
 
+import jsGrammar from '../assets/monaco-grammars/JavaScript.tmLanguage.json?url'
+import cssGrammar from '../assets/monaco-grammars/css.tmLanguage.json?url'
+import htmlGrammar from '../assets/monaco-grammars/html.tmLanguage.json?url'
+import mdGrammar from '../assets/monaco-grammars/markdown.tmLanguage.json?url'
+
 export const registry = new Registry({
   getGrammarDefinition: async (scopeName) => {
     if (scopeName === 'source.js') {
       return {
         format: 'json',
-        content: await (
-          await fetch('src/assets/monaco-grammars/JavaScript.tmLanguage.json')
-        ).text(),
+        content: await (await fetch(jsGrammar)).text(),
       }
     } else if (scopeName === 'source.css') {
       return {
         format: 'json',
-        content: await (
-          await fetch('src/assets/monaco-grammars/css.tmLanguage.json')
-        ).text(),
+        content: await (await fetch(cssGrammar)).text(),
       }
     } else if (scopeName === 'source.html') {
       return {
         format: 'json',
-        content: await (
-          await fetch('src/assets/monaco-grammars/html.tmLanguage.json')
-        ).text(),
+        content: await (await fetch(htmlGrammar)).text(),
       }
     } else {
       return {
         format: 'json',
-        content: await (
-          await fetch('src/assets/monaco-grammars/markdown.tmLanguage.json')
-        ).text(),
+        content: await (await fetch(mdGrammar)).text(),
       }
     }
   },
