7f4a6e9e7e07c91b1d7fdce4c4555340266673e7
Joseph Oliveira <josephvalenteoliveira@gmail.com>
2022-06-04 12:54:17 -0300

    fix: adjustments on the dropdown

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -60,7 +60,7 @@ export function DropdownMenu() {
         <div>
           <Menu.Button className="inline-flex items-center w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
             <img src={logoSvg} className="w-4 inline mr-2" alt="" />
-            Options
+            Menu
           </Menu.Button>
         </div>
         <Transition
@@ -73,7 +73,7 @@ export function DropdownMenu() {
           leaveTo="transform opacity-0 scale-95"
         >
           <Menu.Items className="absolute mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-            <div className="px-1 py-1 ">
+            <div className="px-1 py-1">
               <Menu.Item>
                 {({ active }) => (
                   <button
@@ -82,7 +82,7 @@ export function DropdownMenu() {
                     } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
                     onClick={handleDownloadAsZip}
                   >
-                    <ArchiveBox size={21} className="mr-2" />
+                    <ArchiveBox size={20} className="mr-2" />
                     Download as ZIP
                   </button>
                 )}
