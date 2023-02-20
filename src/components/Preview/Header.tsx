ebe7120b8788a60379b43df4dd30dc008c3da4ba
Mayk Brito <maykbrito@gmail.com>
2023-02-20 13:05:34 -0300

    fix: live reload button in iPhone

--- src/components/Preview/Header.tsx
+++ src/components/Preview/Header.tsx
@@ -37,7 +37,7 @@ export function Header({
 
   return (
     <header
-      className={classNames('bg-zinc-100 w-full h-8 px-4 grid items-center', {
+      className={classNames('bg-zinc-100 w-full h-8 px-4 grid items-center relative', {
         [`grid-cols-floatingPreviewHeader`]: isFloating,
         [`cursor-move`]: canBeDraggable,
         [`hidden`]: isFullscreen,
@@ -89,8 +89,8 @@ export function Header({
       <label 
         title="Habilitar/desabilitar recarregamento automático"
         htmlFor="live-reload" 
-        className="text-xs text-zinc-500 hover:opacity-[1] opacity-[0.4] flex gap-2 absolute right-3">
-        <span className="flex-1 flex justify-end cursor-pointer">Live reload?</span>
+        className="hover:opacity-[1] opacity-[0.4] flex gap-2 absolute right-3 top-1.5">
+        <span className="text-xs text-zinc-500 flex-1 flex justify-end cursor-pointer">Live reload?</span>
         <input
           id="live-reload"
           type="checkbox"
