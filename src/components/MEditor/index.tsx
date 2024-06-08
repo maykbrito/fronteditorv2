fdf902491e690cfdb11a26116d2777576af03cbd
Eduardo Toriani <edutoriani13@gmail.com>
2024-06-08 07:00:32 -0300

    feat: mobile moved editor to bottom

--- src/components/MEditor/index.tsx
+++ src/components/MEditor/index.tsx
@@ -44,11 +44,11 @@ export function MEditor({
   ]
 
   return (
-    <motion.div 
+    <motion.div
       className={classNames("w-screen h-screen overflow-hidden relative sm:flex grid", {
         [`grid-rows-2`]: !isFullscreen,
       })}>
-      <div className={isFullscreen ? `hidden` : `w-full flex flex-col h-full`}>
+      <div className={isFullscreen ? `hidden` : `w-full flex flex-col h-full order-2 sm:order-1`}>
         <nav className="flex items-center gap-1 py-1 px-0 sm:px-4 sm:py-2 bg-[#11111b]">
           {showLogo && (
             <a title="visit the open-source project" href="https://github.com/maykbrito/fronteditorv2" target="_blank" className="text-center px-4">
